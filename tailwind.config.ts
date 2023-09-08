2f6f1329e5b6ea764b5847e10e086f6202408129
Yenoh <tessemayenoh94@gmail.com>
2023-09-08 11:23:55 +0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,23 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        RoyalPurple: "#6743EE",
+        LightBlack: "rgba(0, 0, 0, 0.50)",
+      },
+      screens: {
+        xs: "200px",
+        smmd: "400px",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
